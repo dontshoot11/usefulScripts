4858e0c66a43e54c22126a864a077e3cf330b797
dontshoot11 <52530789+dontshoot11@users.noreply.github.com>
2022-09-01 14:22:18 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -104,7 +104,8 @@ function sectionCheck(target) {
       window.pageYOffset + target.getBoundingClientRect().bottom;
     const windowBottom =
       window.pageYOffset + document.documentElement.clientHeight;
-    const screenCenter = document.documentElement.clientHeight / 2; //это координаты, относительно которых проводится проверка. уменьшая коэф.деления, двигаем вверх, увеличивая, вниз относительно вьюпорта
+    const screenCenter = document.documentElement.clientHeight / 2; 
+    //screenCenter это координаты, относительно которых проводится проверка. уменьшая коэф.деления, двигаем вверх, увеличивая, вниз относительно вьюпорта
     if (
       windowBottom - targetTop > screenCenter &&
       windowBottom < targetBottom + screenCenter
